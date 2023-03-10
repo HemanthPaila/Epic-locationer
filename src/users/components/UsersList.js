48b28b0b0d988a6bd2a31d9831dcdcf23c9b97de
HemanthPaila <pailahemanth2002@gmail.com>
2023-03-10 23:03:15 +0530

    EL-10 Added UserItem and UsersList components

---
+++ src/users/components/UsersList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./UsersList.css";
+import UserItem from "./UserItem";
+const UsersList = (props) => {
+  if (props.items.length === 0) {
+    return (
+      <div className="center">
+        <h2>No users found</h2>
+      </div>
+    );
+  }
+  return (
+    <ul>
+      {props.items.map((user) => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places}
+        />
+      ))}
+    </ul>
+  );
+};
+export default UsersList;
