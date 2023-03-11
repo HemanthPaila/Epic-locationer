55c71f48c8477b285f4d55f9857d59664e7003a2
HemanthPaila <pailahemanth2002@gmail.com>
2023-03-11 23:55:57 +0530

    EL-12 Added styling to user list and user item componemts

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -2,12 +2,12 @@ import React from "react";
 import UsersList from "../components/UsersList";
 const Users = () => {
   const USERS = [
-    // {
-    //   id: "u1",
-    //   name: "max",
-    //   image: "",
-    //   places: 3,
-    // },
+    {
+      id: "u1",
+      name: "max",
+      image: "",
+      places: 3,
+    },
   ];
   return <UsersList items={USERS} />;
 };
